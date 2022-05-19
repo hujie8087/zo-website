bcc6c61129da6832cd1ca4779ae2c021e92bdcce
hujie8087 <80776811@qq.com>
2022-05-19 08:19:01 +0800

    新闻列表

---
+++ front/pages/news/_id.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>news-dtail</div>
+</template>
+
+<script>
+export default {
+  transition: 'fade',
+}
+</script>
+
+<style lang="scss" scoped></style>
--- front/pages/news/index.vue
+++ front/pages/news/index.vue
@@ -18,6 +18,39 @@
           {{ newsType.name }}
         </nuxt-link>
       </div>
+      <div class="news-list">
+        <div v-for="news in newsList" :key="news.id" class="item">
+          <el-row>
+            <el-col :span="16">
+              <div class="title">
+                {{ news.title }}
+              </div>
+              <div class="date">
+                {{ news.createData }}
+              </div>
+              <div class="content">
+                {{ news.content }}
+              </div>
+              <div class="link">
+                <nuxt-link :to="'/news/' + news.id">MORE ></nuxt-link>
+              </div></el-col
+            >
+            <el-col :span="8">
+              <img :src="news.imgUrl" alt="" srcset="" />
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+      <el-pagination
+        class="pagination"
+        background
+        layout="total,prev, pager, next"
+        :current-page.sync="pageParams.currentPage"
+        :page-size="pageParams.pageSize"
+        :total="total"
+        @current-change="handleCurrentChange"
+      >
+      </el-pagination>
     </div>
   </div>
 </template>
@@ -26,6 +59,7 @@
 import DefaultBanner from '../../components/DefaultBanner.vue'
 export default {
   components: { DefaultBanner },
+  transition: 'fade',
   data() {
     return {
       bannerOptions: {
@@ -43,10 +77,33 @@ export default {
         },
       ],
       newsList: [],
+      pageParams: {
+        currentPage: 1,
+        pageSize: 6,
+      },
+      total: 0,
     }
   },
+  created() {
+    this.getNewsList()
+  },
   methods: {
-    getNewsList() {},
+    getNewsList() {
+      this.$http
+        .get('/getNewsList', { current: this.pageParams.currentPage })
+        .then((result) => {
+          this.newsList = result.data
+          this.initStatus = true
+          this.total = result.total
+        })
+        .catch((err) => {
+          this.$message.error(err)
+        })
+    },
+    handleCurrentChange(val) {
+      this.pageParams.currentPage = val
+      this.getNewsList()
+    },
   },
 }
 </script>
@@ -73,6 +130,55 @@ export default {
       color: #fff;
       background-color: #333333;
     }
+  }
+}
+.news-list {
+  .item {
+    margin-bottom: 30px;
+    border-bottom: 1px solid #e9e9e9;
+    padding-bottom: 30px;
+    .title {
+      margin-bottom: 20px;
+      color: #333333;
+      font-size: 16px;
+      font-weight: bold;
+    }
+    .date {
+      margin-bottom: 10px;
+      width: 160px;
+      color: #b7b7b7;
+      font-size: 14px;
+    }
+    .content {
+      line-height: 25px;
+      font-size: 14px;
+      color: #666;
+      height: 75px;
+      max-width: 700px;
+    }
+    a {
+      font-size: 16px;
+      color: #fdc900;
+      font-weight: bold;
+    }
+  }
+}
+.pagination {
+  text-align: center;
+  margin: 50px 0;
+  /deep/ button,
+  /deep/ span:not([class*='suffix']) {
+    font-size: 16px;
+    min-width: 40px;
+    height: 40px;
+    line-height: 40px;
+  }
+  /deep/ .el-pager li {
+    width: 40px;
+    margin: 0 8px;
+    height: 40px;
+    font-size: 16px;
+    line-height: 40px;
   }
 }
 </style>
